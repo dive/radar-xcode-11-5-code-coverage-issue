4ec7aa7e5dfb9e14ae868aec58ddd90128625947
Artem Loenko <artyom.loenko@mac.com>
2020-06-03 12:38:33 +0100

    added a base version of the script to check the coverage

---
+++ check_coverage.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set +e
+
+INCREMENTAL_XCRESULT="./incremental.xcresult"
+WHOLEMODULE_XCRESULT="./wholemodule.xcresult"
+
+function bootstrap {
+    if [ ! -d "CocoaLumberjack" ]; then
+        git clone https://github.com/CocoaLumberjack/CocoaLumberjack.git
+    fi
+}
+
+function cleanUp {
+    rm -Rf "$INCREMENTAL_XCRESULT"
+    rm -Rf "$WHOLEMODULE_XCRESULT"
+    git checkout .
+
+    pushd CocoaLumberjack
+    git checkout .
+    popd
+}
+
+function produceCodeCoverage {
+    rm -Rf build
+    sed -i '' -E "s/^SWIFT_COMPILATION_MODE \= .*$/$2/g" CocoaLumberjack/Configs/Module-Debug.xcconfig
+    xcodebuild test \
+               -project CocoaLumberjack/Tests/Tests.xcodeproj \
+               -scheme "Swift Tests" \
+               -configuration Debug \
+               -resultBundlePath "$1" \
+               -quiet
+}
+
+function showCodeCoverage {
+    printf "%s\n\n" "=== Coverage for $1 ==="
+    xcrun xccov view --only-targets --report "$1"
+}
+
+bootstrap
+cleanUp
+produceCodeCoverage "$INCREMENTAL_XCRESULT" "SWIFT_COMPILATION_MODE = incremental"
+produceCodeCoverage "$WHOLEMODULE_XCRESULT" "SWIFT_COMPILATION_MODE = wholemodule"
+showCodeCoverage $INCREMENTAL_XCRESULT
+showCodeCoverage $WHOLEMODULE_XCRESULT
+cleanUp
